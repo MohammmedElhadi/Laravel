f9223965393751d6cfe26cd10c17a3479341140a
MohammmedElhadi <fm_arneb@esi.dz>
2022-09-30 15:08:14 +0100

    cors *

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,8 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['frontend.auto_parts.local','https://app.safyauto.com' ,'*'],
+    // 'allowed_origins' => ['frontend.auto_parts.local','https://app.safyauto.com' ,'*'],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => ['*'],
 
